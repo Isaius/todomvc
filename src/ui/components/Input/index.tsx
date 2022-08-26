1aaf80c6241103630f24b776dbcc80b9c16806da
Isaias <isaigrec@gmail.com>
2022-08-26 13:27:38 -0300

    make input work properly

--- src/ui/components/Input/index.tsx
+++ src/ui/components/Input/index.tsx
@@ -1,18 +1,36 @@
+import { useState } from 'react'
+
 import useTodo from '../../hooks/useTodo';
 import { Container, InputField } from './styles';
 
 const Input = () => {
   const { handleTodoAddition } = useTodo();
+  const [inputValue, setInputValue] = useState("")
 
-  const handleSubmission = (event: React.FormEvent<HTMLInputElement>) => {
-    const title = event.currentTarget.value;
-    event.currentTarget.value = ""
-    handleTodoAddition(title);
+  const handleKeyboardSubmission = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      handleTodoAddition(inputValue)
+      clearInput()
+    }
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  }
+
+  const clearInput = () => { 
+    setInputValue("")
   }
 
   return (
     <Container className='todo-item'>
-      <InputField type='text' className='todo-input' placeholder='What needs to be done?' onBlur={handleSubmission}/>
+      <InputField
+        type='text'
+        className='todo-input'
+        value={inputValue}
+        placeholder='What needs to be done?'
+        onChange={handleInputChange}
+        onKeyDown={handleKeyboardSubmission} />
     </Container>
   );
 };
